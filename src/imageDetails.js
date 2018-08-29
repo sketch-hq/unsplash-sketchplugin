7c1ac71a23f3c1775b9ae026b7b5f3cb13865d79
Ale M <bomberstudios@gmail.com>
2018-08-29 11:23:06 +0200

    Error handling

--- src/imageDetails.js
+++ src/imageDetails.js
@@ -4,9 +4,19 @@ const Settings = sketch.Settings
 
 export default function imageDetails(context){
   var selection = sketch.getSelectedDocument().selectedLayers
-  selection.forEach(element => {
-    var id = Settings.layerSettingForKey(element, 'unsplash.photo.id')
-    // UI.message(`URL for photo: https://unsplash.com/photos/${id}`)
-    NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString(`https://unsplash.com/photos/${id}`))
-  })
+  if (selection.length > 0) {
+    selection.forEach(element => {
+      var id = Settings.layerSettingForKey(element, 'unsplash.photo.id')
+      if (id) {
+        // UI.message(`URL for photo: https://unsplash.com/photos/${id}`)
+        NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString(`https://unsplash.com/photos/${id}`))
+      } else {
+        // This layer doesn't have an Unsplash photo set, do nothing.
+        // Alternatively, show an explanation of what the user needs to do to make this work…
+        // UI.message(``)
+      }
+    })
+  } else {
+    UI.message(`Please select at least one layer`)
+  }
 }
