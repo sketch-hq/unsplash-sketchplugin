aef384ac34376d71aaee70870fcbe2f183c47a6c
mathieudutour <mathieu@dutour.me>
2018-09-05 11:29:57 -0400

    use new js api + async fetch

--- src/DataProvider.js
+++ src/DataProvider.js
@@ -1,3 +1,9 @@
+const os = require('os')
+const path = require('path')
+const util = require('util')
+const fs = require('@skpm/fs')
+const sketch = require('sketch')
+
 const API_KEY = 'bfd993ac8c14516588069b3fc664b216d0e20fb9b9fa35aa06fcc3ba6e0bc703'
 const API_ENDPOINT = 'https://api.unsplash.com'
 const action = '/photos/random'
@@ -8,10 +14,10 @@ const apiOptions = {
   }
 }
 
-const sketch = require('sketch')
-const DataSupplier = sketch.DataSupplier
-const UI = sketch.UI
-const Settings = sketch.Settings
+const { DataSupplier, UI, Settings } = sketch
+
+const SETTING_KEY = 'unsplash.photo.id'
+const FOLDER = path.join(os.tmpdir(), 'com.sketchapp.unsplash-plugin')
 
 export function onStartup () {
   DataSupplier.registerDataSupplier('public.image', 'Unsplash Random Photo', 'SupplyPhoto')
@@ -19,20 +25,29 @@ export function onStartup () {
 
 export function onShutdown () {
   DataSupplier.deregisterDataSuppliers()
-  // TODO Remove temporary files
+  try {
+    fs.rmdirSync(FOLDER)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 export function onSupplyPhoto (context) {
   let dataKey = context.data.key
-  let items = context.data.layers.length > 0 ? context.data.layers : context.data.overrides
+  const items = util.toArray(context.data.items).map(sketch.fromNative)
   items.forEach((item, index) => setImageFor(item, index, dataKey))
 }
 
-export default function onImageDetails (context) {
+export default function onImageDetails () {
   var selection = sketch.getSelectedDocument().selectedLayers
   if (selection.length > 0) {
     selection.forEach(element => {
-      var id = Settings.layerSettingForKey(element, 'unsplash.photo.id')
+      var id = Settings.layerSettingForKey(element, SETTING_KEY) || (
+        element.type === 'SymbolInstance' &&
+        element.overrides
+          .map(o => Settings.layerSettingForKey(o, SETTING_KEY))
+          .find(s => !!s)
+      )
       if (id) {
         NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString(`https://unsplash.com/photos/${id}`))
       } else {
@@ -47,68 +62,80 @@ export default function onImageDetails (context) {
 }
 
 function setImageFor (item, index, dataKey) {
-  // console.log(`setImageFor ${item}`)
   let layer
-  if (item.className() == 'MSDataOverride') {
-    layer = item.symbolInstance() // or item.affectedLayer(), but both of them are not really what we need… Check `MSOverrideRepresentation` to get the true size of the affected layer after being resized on the Symbol instance
+  if (item.type === 'DataOverride') {
+    layer = item.symbolInstance // or item.override.affectedLayer, but both of them are not really what we need… Check `MSOverrideRepresentation` to get the true size of the affected layer after being resized on the Symbol instance
   } else {
     layer = item
   }
 
   let orientation
-  if (layer.frame().width() > layer.frame().height()) {
+  if (layer.frame.width > layer.frame.height) {
     orientation = 'landscape'
   }
-  if (layer.frame().width() < layer.frame().height()) {
+  if (layer.frame.width < layer.frame.height) {
     orientation = 'portrait'
   }
-  if (layer.frame().width() === layer.frame().height()) {
+  if (layer.frame.width === layer.frame.height) {
     orientation = 'squarish'
   }
+
   let url = API_ENDPOINT + action + '?client_id=' + API_KEY + '&count=1&orientation=' + orientation + '&collections=' + collectionId
 
   UI.message('🕑 Downloading…')
   fetch(url, apiOptions)
-    .then(response => response.text())
-    .then(text => process(text, dataKey, index, item))
+    .then(response => response.json())
+    .then(json => process(json, dataKey, index, item))
     .catch(e => console.log(e))
 }
 
 function process (unsplashJSON, dataKey, index, item) {
-  let data = JSON.parse(unsplashJSON)[0]
-  // console.log(data)
-  let path = getImageFromURL(data.urls.regular)
-  let downloadLocation = data.links.download_location + '?client_id=' + API_KEY
-  DataSupplier.supplyDataAtIndex(dataKey, path, index)
-  // TODO if layer belongs to a Symbol, we shouldn't set the data on the layer, but on the instance, storing a reference to the override to use it later
-  // console.log(`We're setting an ID on ${item}`)
-  if (item.className() === 'MSDataOverride') {
-    // let overrideID = item.overrideIdentifier()
-  } else {
-    // This is a regular layer
-    Settings.setLayerSettingForKey(item, 'unsplash.photo.id', data.id)
-  }
-  fetch(downloadLocation, apiOptions)
-    .then(UI.message('📷 by ' + data.user.name + ' on Unsplash'))
-    .catch(e => console.log(e))
+  let data = unsplashJSON[0]
+
+  // supply the data
+  getImageFromURL(data.urls.regular).then(imagePath => {
+    if (!imagePath) {
+      // TODO: something wrong happened, show something to the user
+      return
+    }
+    DataSupplier.supplyDataAtIndex(dataKey, imagePath, index)
+
+    // store where the image comes from
+    Settings.setLayerSettingForKey(item, SETTING_KEY, data.id)
+
+    // show the name of the photographer
+    let downloadLocation = data.links.download_location + '?client_id=' + API_KEY
+    fetch(downloadLocation, apiOptions)
+      .then(UI.message('📷 by ' + data.user.name + ' on Unsplash'))
+      .catch(e => console.log(e))
+  })
 }
 
 function getImageFromURL (url) {
-  let request = NSURLRequest.requestWithURL(NSURL.URLWithString(url))
-  let imageData = NSURLConnection.sendSynchronousRequest_returningResponse_error(request, null, null)
-  if (imageData) {
+  return fetch(url)
+    .then(res => res.blob())
     // TODO: use imageData directly, once #19391 is implemented
-    return saveTempFileFromImageData(imageData)
-  } else {
-    return context.plugin.urlForResourceNamed('placeholder.png').path()
-  }
+    .then(saveTempFileFromImageData)
+    .catch((err) => {
+      console.error(err)
+      return context.plugin.urlForResourceNamed('placeholder.png').path()
+    })
 }
 
 function saveTempFileFromImageData (imageData) {
-  let guid = NSProcessInfo.processInfo().globallyUniqueString()
-  let folder = NSTemporaryDirectory().stringByAppendingPathComponent('com.sketchapp.unsplash-plugin')
-  let path = folder.stringByAppendingPathComponent(`${guid}.jpg`)
-  NSFileManager.defaultManager().createDirectoryAtPath_withIntermediateDirectories_attributes_error(folder, false, nil, nil)
-  imageData.writeToFile_atomically(path, true)
-  return path
+  const guid = NSProcessInfo.processInfo().globallyUniqueString()
+  const imagePath = path.join(FOLDER, `${guid}.jpg`)
+  try {
+    fs.mkdirSync(FOLDER)
+  } catch (err) {
+    // probably because the folder already exists
+    // TODO: check that it is really because it already exists
+  }
+  try {
+    fs.writeFileSync(imagePath, imageData, 'NSData')
+    return imagePath
+  } catch (err) {
+    console.error(err)
+    return undefined
+  }
 }
